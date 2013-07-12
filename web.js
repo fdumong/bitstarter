3d20c66f12448ac4ac6953edd27f59dcce61b8f7
Ubuntu <ubuntu@ip-172-31-28-4.us-west-2.compute.internal>
2013-07-12 19:02:39 +0000

    web.js is modified

--- web.js
+++ web.js
@@ -1,6 +1,18 @@
 var express = require('express');
 var fs  = require('fs');
 var app = express.createServer(express.logger());
+var filecontent;
+var content="";
+
+try{
+ filecontent = fs.readFileSync('index.html','utf8');
+}
+catch(err){
+ console.log(err)
+} 
+
+if(filecount){
+content+=filecontent;
 
 app.get('/',function(request,response){
    response.send('Hello Nasa');
@@ -10,4 +22,4 @@ var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });  
-
+}
